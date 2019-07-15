3bd45d6ba71f77750beecaace7d4c0004da5b6e4
Kevin Kent <kevin.kent1505@gmail.com>
2019-07-15 16:29:55 +0700

    added hearts

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
           title: 'Name Generator',
+          theme: ThemeData(
+            primaryColor: Colors.white,
+          ),
           home: RandomWords(),
         );
   }
@@ -22,12 +25,48 @@ class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final Set<WordPair> _saved = Set<WordPair>();
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );                       // ... to here.
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list),onPressed: _pushSaved ,)
+        ],
       ),
+
       body: _buildSuggestions(),
     );
   }
@@ -42,7 +81,6 @@ class RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
-          final indexString = i.toString();
           return _buildRow(_suggestions[index]);
         });
   }
@@ -57,6 +95,15 @@ class RandomWordsState extends State<RandomWords> {
       trailing: Icon(alreadySaved? Icons.favorite:Icons.favorite_border,
       color: alreadySaved ? Colors.red : null,
       ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
